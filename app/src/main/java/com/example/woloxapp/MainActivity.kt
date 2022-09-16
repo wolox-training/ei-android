5786c187684646904959a97411eab84bc3f6fb9c
Esteban Indiveri <esteban.indiveri@gmail.com>
2022-09-16 16:32:11 -0300

    added fragment login ui

--- app/src/main/java/com/example/woloxapp/MainActivity.kt
+++ app/src/main/java/com/example/woloxapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.woloxapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.example.woloxapp.ui.login.LoginFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
+            WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION
+        )
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        // TODO add authManager
+        val auth = true
+        if (auth) supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, LoginFragment()).commit()
     }
 }
---
+++ app/src/main/java/com/example/woloxapp/ui/login/LoginFragment.kt
@@ -0,0 +1,22 @@
+package com.example.woloxapp.ui.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.woloxapp.databinding.LoginFragmentBinding
+
+class LoginFragment : Fragment() {
+    private var _binding: LoginFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = LoginFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
