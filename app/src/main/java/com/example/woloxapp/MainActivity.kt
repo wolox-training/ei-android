c64d488c5793c7812f2fec4d21172e37d75ca7b8
Esteban Indiveri <esteban.indiveri@gmail.com>
2022-09-19 15:23:37 -0300

    updated and fixed comments

--- app/src/main/java/com/example/woloxapp/MainActivity.kt
+++ app/src/main/java/com/example/woloxapp/MainActivity.kt
@@ -2,7 +2,8 @@ package com.example.woloxapp
 
 import android.os.Bundle
 import android.view.View
-import android.view.WindowManager
+import android.view.WindowManager.LayoutParams
+import android.view.WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION
 import androidx.appcompat.app.AppCompatActivity
 import com.example.woloxapp.ui.login.LoginFragment
 
@@ -11,11 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
-        getWindow().setFlags(
-            WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
-            WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION
-        )
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        with(LayoutParams()) { getWindow().setFlags(FLAG_TRANSLUCENT_NAVIGATION, FLAG_TRANSLUCENT_NAVIGATION) }
         // TODO add authManager
         val auth = true
         if (auth) supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, LoginFragment()).commit()
--- app/src/main/java/com/example/woloxapp/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/woloxapp/ui/login/LoginFragment.kt
@@ -4,19 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.woloxapp.databinding.LoginFragmentBinding
 
 class LoginFragment : Fragment() {
-    private var _binding: LoginFragmentBinding? = null
-    private val binding get() = _binding!!
-
+    private lateinit var _binding: LoginFragmentBinding
+    private val binding get() = _binding
+    private fun uppercaseString(_button: Button): String {
+        val uppercaseStringButton = _button.text.toString().uppercase()
+        val tempButton = uppercaseStringButton.also { _button.text = it }
+        return tempButton
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = LoginFragmentBinding.inflate(inflater, container, false)
+        val loginBtn = binding.loginBtn
+        val signupBtn = binding.signupBtn
+        uppercaseString(loginBtn)
+        uppercaseString(signupBtn)
         return binding.root
     }
 }
