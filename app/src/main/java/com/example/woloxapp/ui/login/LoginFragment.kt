9b3382cdb54e2a7c1d9924b0bacc50b7a00e1b8b
Esteban Indiveri <esteban.indiveri@gmail.com>
2022-09-20 13:53:01 -0300

    updated with comments

--- app/src/main/java/com/example/woloxapp/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/woloxapp/ui/login/LoginFragment.kt
@@ -11,21 +11,12 @@ import com.example.woloxapp.databinding.LoginFragmentBinding
 class LoginFragment : Fragment() {
     private lateinit var _binding: LoginFragmentBinding
     private val binding get() = _binding
-    private fun uppercaseString(_button: Button): String {
-        val uppercaseStringButton = _button.text.toString().uppercase()
-        val tempButton = uppercaseStringButton.also { _button.text = it }
-        return tempButton
-    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = LoginFragmentBinding.inflate(inflater, container, false)
-        val loginBtn = binding.loginBtn
-        val signupBtn = binding.signupBtn
-        uppercaseString(loginBtn)
-        uppercaseString(signupBtn)
         return binding.root
     }
 }
